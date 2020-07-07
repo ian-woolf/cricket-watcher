49811b4f4ff5829578237bf9e2b3c13b1e18ecc5
Ian Woolf <dev@ianwoolf.me.uk>
2020-07-07 23:27:54 +0100

    Start breaking up resolvers.ts. First, split data into a separate file.

---
+++ src/data.ts
@@ -0,0 +1,17 @@
+export const matches = [
+    require('../data/matches/2181_43648.json'),
+    require('../data/matches/2181_43652.json'),
+    require('../data/matches/2181_43658.json'),
+    require('../data/matches/2181_43664.json'),
+    require('../data/matches/2181_43668.json'),
+    require('../data/matches/2181_43676.json')
+  ]
+
+let teamsMap = new Map();
+matches.forEach(match => {
+  let matchObj = match.body.match;
+  teamsMap.set(matchObj.homeTeam.id, { id: matchObj.homeTeam.id, name: matchObj.homeTeam.name });
+  teamsMap.set(matchObj.awayTeam.id, { id: matchObj.awayTeam.id, name: matchObj.awayTeam.name });
+})
+
+export const teams = Array.from(teamsMap, ([name, value]) => value);
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,21 +1,5 @@
 const { UserInputError } = require('apollo-server-lambda');
-
-const matches = [
-  require('../data/matches/2181_43648.json'),
-  require('../data/matches/2181_43652.json'),
-  require('../data/matches/2181_43658.json'),
-  require('../data/matches/2181_43664.json'),
-  require('../data/matches/2181_43668.json'),
-  require('../data/matches/2181_43676.json')
-]
-
-let teamsMap = new Map();
-matches.forEach(match => {
-  let matchObj = match.body.match;
-  teamsMap.set(matchObj.homeTeam.id, { id: matchObj.homeTeam.id, name: matchObj.homeTeam.name });
-  teamsMap.set(matchObj.awayTeam.id, { id: matchObj.awayTeam.id, name: matchObj.awayTeam.name });
-})
-const teams = Array.from(teamsMap, ([name, value]) => value);
+const { matches, teams } = require('./data');
 
 // TODO: this is exactly the same as the graphQL schema
 // any way to share a source of truth?
